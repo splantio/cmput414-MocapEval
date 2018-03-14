5e701b742135a428ae198167f60700d76ee5f8af
Spencer <splant.10@gmail.com>
2018-03-13 22:17:57 -0600

    two cameras with an optional public motion for quick demo

--- CMPUT414-Mocap/Assets/Scripts/PlayerAnimator.cs
+++ CMPUT414-Mocap/Assets/Scripts/PlayerAnimator.cs
@@ -7,13 +7,21 @@ public class PlayerAnimator : MonoBehaviour {
 
     public AnimatorController animatorController;
     private AnimatorState defaultState;
+    public Motion optionalMotion;
     
 	void Start () {
         // State Machine
         AnimatorStateMachine rootStateMachine = animatorController.layers[0].stateMachine;
         rootStateMachine.states = null;
         this.defaultState = rootStateMachine.AddState("defaultState", new Vector3(400f, 200f, 0f));
-        animatorController.SetStateEffectiveMotion(defaultState, Resources.Load<AnimationClip>("RawMocapData/Animations/Running/RunSlide_ToRight_1"));
+        if (optionalMotion)
+        {
+            animatorController.SetStateEffectiveMotion(defaultState, optionalMotion);
+        } else
+        {
+            animatorController.SetStateEffectiveMotion(defaultState, Resources.Load<AnimationClip>("RawMocapData/Animations/Running/RunSlide_ToRight_1"));
+        }
+       
 
         
 	}
