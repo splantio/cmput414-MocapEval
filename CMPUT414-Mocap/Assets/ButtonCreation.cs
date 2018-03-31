c8c14a394520c240d8354dbaeec763270c810d2a
alexczeto <alexczeto@yahoo.ca>
2018-03-31 13:28:42 -0600

    FUn with UI

--- CMPUT414-Mocap/Assets/ButtonCreation.cs
+++ CMPUT414-Mocap/Assets/ButtonCreation.cs
@@ -6,13 +6,15 @@ using UnityEngine.UI;
 public class ButtonCreation : MonoBehaviour {
 	public FileNames fN;
 	public PlayerAnimator pA;
+    public Transform contentPanel;
 	// Use this for initialization
 	void Start () {
 		fN = new FileNames ();
 		for (int i = 0; i < fN.fileString.Count;  i++) {
 			//Create the number of buttons for the number of files avilable 
-			GameObject bttn = Instantiate (Resources.Load ("Prefab/Button"),gameObject.transform) as GameObject; 
-			//bttn.transform.parent = gameObject.transform;
+			GameObject bttn = Instantiate (Resources.Load ("Prefab/Button"),gameObject.transform) as GameObject;
+
+            bttn.transform.SetParent(contentPanel);
 			ButtonScript bttnScript = bttn.GetComponent<ButtonScript> ();
 			bttnScript.setFbxFile (fN.fileString [i]);
 			//bttnScript.setPlayerAnimator (pA);
